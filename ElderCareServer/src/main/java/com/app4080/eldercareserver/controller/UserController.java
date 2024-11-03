51792243c7b87375c90a903bd6bc013fd80b37ee
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2024-11-03 23:22:31 +0300

    added UserController.java

---
+++ ElderCareServer/src/main/java/com/app4080/eldercareserver/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.app4080.eldercareserver.controller;
+
+import com.app4080.eldercareserver.entity.User;
+import com.app4080.eldercareserver.service.Impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try{
+            User createdUser = userService.createUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser(@RequestBody User user) {
+        try{
+            userService.deleteUser(user);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+}
