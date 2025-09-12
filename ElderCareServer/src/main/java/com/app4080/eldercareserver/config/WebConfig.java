a527698cac8fd788783b7107f87300feab9e64b3
tmjoris <tangajoris@gmail.com>
2025-09-13 01:37:50 +0300

    changed allowedOrigins

--- ElderCareServer/src/main/java/com/app4080/eldercareserver/config/WebConfig.java
+++ ElderCareServer/src/main/java/com/app4080/eldercareserver/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://eldercarekali.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
